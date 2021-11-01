1a51b609753b99a5883b9b54c9c6c8ebd9ec9cce
Jon Galloway <jongalloway@gmail.com>
2021-11-01 10:57:45 -0700

    Added seed data

--- PizzaStore/PizzaDb.cs
+++ PizzaStore/PizzaDb.cs
@@ -7,4 +7,10 @@ public class PizzaDb : DbContext
 {
     public PizzaDb(DbContextOptions options) : base(options) { }
     public DbSet<Pizza> Pizzas { get; set; }
+
+    protected override void OnModelCreating (ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pizza>().HasData(
+            new Pizza { Id = 1, Name = "Pepperoni", Description = "Classic Pepperoni Pizza"});
+    }
 }
