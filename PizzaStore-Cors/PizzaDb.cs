3e30fdf2489225918bff150b48f17a9ea1244726
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-08-18 10:06:47 -0700

    separate pizza from pizza-cors

---
+++ PizzaStore-Cors/PizzaDb.cs
@@ -0,0 +1,16 @@
+using PizzaStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Data;
+
+public class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+
+    protected override void OnModelCreating (ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pizza>().HasData(
+            new Pizza { Id = 1, Name = "Pepperoni", Description = "Classic Pepperoni Pizza"});
+    }
+}
